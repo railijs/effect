d790885967eee89d019c188139f0fa69c12ef927
railijs <railijsgrieznis@gmail.com>
2023-10-26 09:54:04 +0300

    yes

---
+++ src/ToDo.js
@@ -0,0 +1,12 @@
+function ToDo(props) {
+  return (
+    <div>
+      <p>userId="1"</p>
+      <p>id: 1</p>
+      <p>delectus aut autem</p>
+      <input type="checkbox"></input>
+    </div>
+  );
+}
+
+export default ToDo;
